3958d18f47e3d3b7d7c5c9152072ab991500c305
josephoakes12-afk <joemickey12@gmail.com>
2026-02-10 17:41:46 +0000

    Refactor code structure for improved readability and maintainability

---
+++ assets/site-config.2d266e56.js
@@ -0,0 +1,25 @@
+window.SITE_CONFIG = {
+  brandName: "Elysium By Georgie Rose",
+  email: "Elysium.pj@gmail.com",
+  phoneDisplay: "07361 497192",
+  phoneTel: "+447361497192",
+  instagramUrl: "https://www.instagram.com/elysium_by_georgierose/",
+  facebookUrl: "https://www.facebook.com/profile.php?id=61571925904476",
+  tiktokUrl: "https://www.tiktok.com/@elysium_by_georgierose?is_from_webapp=1&sender_device=pc",
+  social: {
+    instagramUrl: "https://www.instagram.com/elysium_by_georgierose/",
+    facebookUrl: "https://www.facebook.com/profile.php?id=61571925904476",
+    tiktokUrl: "https://www.tiktok.com/@elysium_by_georgierose?is_from_webapp=1&sender_device=pc",
+    instagramEmbedPosts: [
+      "https://www.instagram.com/p/DOTVxIxiJK4/",
+      "https://www.instagram.com/p/DPlmWWcCL65/",
+      "https://www.instagram.com/p/DJELSU3oKns/"
+    ]
+  },
+  bookingUrl: "",
+  contactEmail: "Elysium.pj@gmail.com",
+  contactPhone: "07361 497192",
+  locationText: "Base: 15 Young Street, Cheadle. Georgie travels for pop-ups and may be coming to your area soon.",
+  googleReviewsUrl: "https://share.google/EBIZEN8UOwQqzRysz",
+  formspreeEndpoint: "https://formspree.io/f/xvzbgojb"
+};
